c3724b89b39233211621fb31bef57a500378e5e8
Joel-Green <joelgreen737@gmail.com>
2020-12-22 10:01:02 +0530

    created seperated autostart file

---
+++ autostart.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+/home/shaw/.scripts/battery_notify &
+autorandr -c
+# stalonetray &
+sxhkd &
+picom &
+flameshot &
+nm-applet --no-agent &
+
+/usr/bin/xmodmap /home/shaw/.Xmodmap &
+
+# feh --bg-fill /home/shaw/Downloads/wallpaper_redcirclesun.jpg
+feh --bg-fill /home/shaw/.wallpapers/wallpaper_aenami_witcher.jpg
+
+
